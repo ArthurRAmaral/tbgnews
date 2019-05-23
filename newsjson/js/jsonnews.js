5b8c2e733f66b813425d11ff871bd05974b0dd68
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-05-23 17:22:05 -0300

    att

---
+++ newsjson/js/jsonnews.js
@@ -0,0 +1,68 @@
+var bdnews = {
+    "data: [
+    {
+        "link"= "nt1.html",
+        "type"= "Esportes",
+        "img"= "http://lorempixel.com/480/227/sports/3",
+        "title"= "Notícias 1"
+        "text"= "Está é a noticia demonstrativa para avaliação.",
+        "date"= "30/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Esportes",
+        "img"= "http://lorempixel.com/480/227/sports/6",
+        "title"= "Notícias 2"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus, amet sore doloribus aperiam tempora et!",
+        "date"= "29/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Esportes",
+        "img"= "http://lorempixel.com/480/227/sports/7/",
+        "title"= "Notícias 3"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus, ampora et!",
+        "date"= "28/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Alimentação",
+        "img"= "http://lorempixel.com/480/227/food/6/",
+        "title"= "Notícias 4"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus,ra et!",
+        "date"= "27/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Alimentação",
+        "img"= "http://lorempixel.com/480/227/food/7/",
+        "title"= "Notícias 5"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus,ribus aperiam tempora et!",
+        "date"= "26/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Natureza",
+        "img"= "http://lorempixel.com/480/227/animals/5/",
+        "title"= "Notícias 6"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus,!",
+        "date"= "25/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Natureza",
+        "img"= "http://lorempixel.com/480/227/nature/5/",
+        "title"= "Notícias 7"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus,!",
+        "date"= "24/03/2019"
+    },
+    {
+        "link"= "#",
+        "type"= "Esportes",
+        "img"= "http://lorempixel.com/output/sports-q-c-480-220-5.jpg",
+        "title"= "Notícias 8"
+        "text"= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum voluptas doloribus,!",
+        "date"= "23/03/2019"
+    }
+]
+}
